ef87726ef2e7383e991d87eea3b16a7fc83dc495
Pawel <236691@edu.p.lodz.pl>
2023-11-23 09:09:40 +0100

    initialized test class

---
+++ test_main.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+
+    # case 01-001
+    def test_1(self):
+        self.fail()
+
+    # case 01-002
+    def test_2(self):
+        self.fail()
+
+    # case 01-003
+    def test_3(self):
+        self.fail()
+
+    # case 01-004
+    def test_4(self):
+        self.fail()
+
+    # case 01-005
+    def test_5(self):
+        self.fail()
+
+    # case 01-006
+    def test_6(self):
+        self.fail()
+
+    # case 01-007
+    def test_7(self):
+        self.fail()
+
+    # case 01-008
+    def test_8(self):
+        self.fail()
+
+    # case 01-009
+    def test_9(self):
+        self.fail()
+
+    # case 01-010
+    def test_10(self):
+        self.fail()
