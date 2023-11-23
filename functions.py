9911c4046fe681e9ca5a270c1f7a29d9fabeca2d
JulianWoroniecki <236702@edu.p.lodz.pl>
2023-11-23 13:49:50 +0100

    Refactor control_panel method

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-
 
 class Functions():
     def login_method(driver):
@@ -22,6 +22,16 @@ class Functions():
         time.sleep(2)
 
     def login_method_waiting(driver):
+        try:
+            navigation_button = WebDriverWait(driver, 2).until(
+                EC.visibility_of_element_located((By.XPATH, '/html/body/app-root/app-header/nav/div/button/span'))
+            )
+            navigation_button.click()
+        except TimeoutException:
+            print("Navigation button not found.")
+
+        time.sleep(1)
+
         sign_in_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//a[@data-test="nav-sign-in"]'))
         )
@@ -42,11 +52,16 @@ class Functions():
         )
         login_button.click()
 
-    def product_control_panel(driver):
-        admin_menu = driver.find_element(By.ID, 'admin-menu')
+    def control_panel(self, driver, button_name):
+        # Available button_name:
+            # dashboard, brands, categories, products, orders, users, messages, settings
+        admin_menu = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, 'admin-menu'))
+        )
         admin_menu.click()
-        time.sleep(2)
 
-        products_menu = driver.find_element(By.XPATH, '//a[@routerlink="/admin/products"]')
+        products_menu = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//a[@routerlink="/admin/' + button_name + '"]'))
+        )
         products_menu.click()
         time.sleep(2)
