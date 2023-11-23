e668a8e70c2b227d389db81b2c63c5a6f6f88ed9
Pawel <236691@edu.p.lodz.pl>
2023-11-23 14:07:12 +0100

    first test

--- test_main.py
+++ test_main.py
@@ -2,6 +2,22 @@ from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from functions import Functions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def click_brands(driver):
+    # click BRANDS button
+    dropdown = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, 'admin-menu'))
+    )
+    dropdown.click()
+
+    brands_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//a[@data-test="nav-admin-brands"]'))
+    )
+    brands_button.click()
 
 
 class Test(TestCase):
@@ -20,8 +36,24 @@ class Test(TestCase):
         driver = self.set_up()
 
         # click BRANDS button
+        click_brands(driver)
 
-        # check list of brands
+        # test visibility of brands
+        table = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'table-hover'))
+        )
+
+        # check if the "Id," "Name," and "Slug" headers are present
+        headers = table.find_elements(By.TAG_NAME, 'th')
+        header_texts = [header.text for header in headers]
+
+        assert "Id" in header_texts, "'Id' column not found"
+        assert "Name" in header_texts, "'Name' column not found"
+        assert "Slug" in header_texts, "'Slug' column not found"
+
+        # Check if there are rows in the table
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        assert len(rows) > 0, "Table has no rows"
 
     # case 02-001
     def test_add_brand(self):
@@ -29,6 +61,7 @@ class Test(TestCase):
         driver = self.set_up()
 
         # click BRANDS button
+        click_brands(driver)
 
         # click ADD BRAND button
 
